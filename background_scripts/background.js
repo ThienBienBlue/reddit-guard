54a06d5a75dc4132fb4f5bd3afa051d6f5f6a293
Thien <thiencca@gmail.com>
2019-09-09 19:16:48 -0700

    Added Reddit blocking to the background script. Changed the icon to one drawn by myself of a certain meme.

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,16 +1,36 @@
-const total_time = 25;
-const update_interval = 1000;
+'use strict';
+
+const total_time = 25;  // In minutes.
+const update_interval = 1000;  // One second.
 
 var mins = 0;
 var secs = 0;
 
 var timer_active = false;
+var registered = null;
+
+async function register_script() {
+    registered = await browser.contentScripts.register({
+        matches: ["*://*.reddit.com/*"],
+        js: [{
+          code: "document.body.innerHTML = '<h1>Stop going on Reddit.<h1>'"
+        }],
+        runAt: "document_idle"
+    });
+}
 
 function start_timer() 
     /* Starts a 25 min timer. */
 {
     secs = total_time * 60;
     decrement();
+}
+
+function unregister_script() {
+    if (registered) {
+        registered.unregister();
+        registered = null;
+    }
 }
 
 function decrement()
@@ -31,17 +51,19 @@ function decrement()
 function stop_timer()
     /* Stops the timer early. */
 {
+    unregister_script();
     timer_active = false;
     secs = 0;
     browser.runtime.sendMessage({
         time_left: "00"
-    })
+    });
 }
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.call === "begin timer") {
         if (!timer_active) {
             start_timer();
+            register_script();
         }
         timer_active = true;
     }
